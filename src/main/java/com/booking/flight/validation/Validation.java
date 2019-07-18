3960b650bf7f8c7826b427c2a4803b8b43960c54
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-18 15:02:27 +0530

    added validation class

---
+++ src/main/java/com/booking/flight/validation/Validation.java
@@ -0,0 +1,19 @@
+package com.booking.flight.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.booking.flight.repository.FlightRepository;
+
+@Component
+public class Validation {
+	
+	public boolean validateFlightByAvailableSeats(Long availableSeats , Long requestedSeats) {
+				
+	if(availableSeats !=  null && requestedSeats != null && availableSeats <= requestedSeats) {
+		return true;
+	}
+		return false;
+	}
+
+}
