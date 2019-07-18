32cc3ab66c2afcaf7f1c0c9bec3695d484c0f178
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-18 11:12:38 +0530

    updated controller

---
+++ src/main/java/com/booking/fight/validation/InvalidFlightDetailsException.java
@@ -0,0 +1,31 @@
+package com.booking.fight.validation;
+
+public class InvalidFlightDetailsException extends Exception {
+
+	public InvalidFlightDetailsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidFlightDetailsException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidFlightDetailsException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidFlightDetailsException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidFlightDetailsException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
--- src/main/java/com/booking/flight/controller/FlightController.java
+++ src/main/java/com/booking/flight/controller/FlightController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.booking.fight.model.FlightModel;
+import com.booking.fight.validation.InvalidFlightDetailsException;
+import com.booking.flight.entity.Flight;
 import com.booking.flight.service.FlightService;
 
 @RestController
@@ -19,17 +24,29 @@ public class FlightController {
 
 	FlightService flightService;
 	
+	private final static String ERR_MSG = " INVALID DATA, MISSING PARAMETER : ";
+	
 	@GetMapping("/allFlights")
-	public ResponseEntity<?> searchFlights(@ResponseBody FlightModel flightModel)
+	public ResponseEntity<?> searchFlights(@RequestBody FlightModel flightModel)
 	{
-		validateFlightDetails(flightModel);
+		try
+		{
+			validateFlightDetails(flightModel);
+		}
+		catch(InvalidFlightDetailsException e)
+		{
+			
+		}
 		
-		return new ResponseEntity<List<Flight>>(flightService.searchFlights(flightModel,HttpStatus.OK);
+		
+		return new ResponseEntity<List<FlightModel>>(flightService.searchFlights(flightModel),HttpStatus.OK);
 		
 	}
 	
-	private static void validateFlightDetails(FlightModel flightModel)
+	private static void validateFlightDetails(FlightModel flightModel) throws InvalidFlightDetailsException
 	{
+		if(StringUtils.isEmpty(flightModel.getSource()))
+				throw new InvalidFlightDetailsException(ERR_MSG+" SOURCE");
 		
 		
 	}
