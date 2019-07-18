1cca5c6deba2880d5143caf0c0d95bee737de78a
hiswapgit <hiswap21@gmail.com>
2019-07-18 10:56:48 +0530

    Respository added

--- src/main/java/com/booking/flight/repository/FlightRepository.java
+++ src/main/java/com/booking/flight/repository/FlightRepository.java
@@ -1,5 +1,16 @@
 package com.booking.flight.repository;
 
-public class FlightRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.booking.flight.entity.Flight;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long>{
+
+	public Optional<List<Flight>>findBySourceAndDestination(String source,String destinaton);
+	
 }
