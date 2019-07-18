bcdd57afef784eb7cef6c8c944449df194598e2d
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-18 11:07:23 +0530

    modified flight service

--- src/main/java/com/booking/flight/service/FlightService.java
+++ src/main/java/com/booking/flight/service/FlightService.java
@@ -1,11 +1,15 @@
 package com.booking.flight.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.booking.fight.model.FlightModel;
 import com.booking.flight.entity.Flight;
 import com.booking.flight.repository.FlightRepository;
 
@@ -17,17 +21,28 @@ public class FlightService {
 	
 	public List<FlightModel> searchFlights(FlightModel model){
 		
-		String source;
+		String source = model.getSource();
 		
-		String destination;
-		
-		LocalDate date ;
+		String destination = model.getDeparture();
 		
 		//need to change DTO to entity
 		
-		List<Flight> flights = flightRepository.findBySourceAndDestinationAndTime(source, destination, date);
+		Flight flight =  new Flight();
+		BeanUtils.copyProperties(model, flight);
 		
-		return  flights;
+		Optional<List<Flight>> flights = flightRepository.findBySourceAndDestination(source, destination);
+		
+		
+		if(flights.isPresent())
+		{
+			List<FlightModel> models = new ArrayList<FlightModel>();
+			BeanUtils.copyProperties(flights.get(), models);
+			
+			return models;
+		}			
+		else {
+			return new ArrayList<FlightModel>();
+		}
 		
 	}
 
