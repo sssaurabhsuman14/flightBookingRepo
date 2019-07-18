5989f4939c0fe7b29e22ced3a61ee42a020462e3
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-18 15:08:12 +0530

    added fare utils

---
+++ src/main/java/com/booking/flight/utils/FareUtils.java
@@ -0,0 +1,12 @@
+package com.booking.flight.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FareUtils {
+	
+	public Double calculateTotalFare(Double fare, Double numberOfPassengers) {
+		return (fare*numberOfPassengers);
+	}
+
+}
