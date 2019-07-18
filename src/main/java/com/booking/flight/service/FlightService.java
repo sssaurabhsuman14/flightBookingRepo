1960e9d985d60189ffe028fdf869f1f2a63a45e3
saurabh suman <sssaurabhsuman@gmail.com>
2019-07-18 10:39:20 +0530

    basic structure for flight service

--- src/main/java/com/booking/flight/service/FlightService.java
+++ src/main/java/com/booking/flight/service/FlightService.java
@@ -1,5 +1,34 @@
 package com.booking.flight.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.booking.flight.entity.Flight;
+import com.booking.flight.repository.FlightRepository;
+
+@Service
 public class FlightService {
+	
+	@Autowired
+	FlightRepository flightRepository;
+	
+	public List<FlightModel> searchFlights(FlightModel model){
+		
+		String source;
+		
+		String destination;
+		
+		LocalDate date ;
+		
+		//need to change DTO to entity
+		
+		List<Flight> flights = flightRepository.findBySourceAndDestinationAndTime(source, destination, date);
+		
+		return  flights;
+		
+	}
 
 }
