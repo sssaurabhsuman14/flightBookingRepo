25bd580b12b6808c668d004ed863e6d5046b354f
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-19 11:22:05 +0530

    booking changes

--- src/main/java/com/booking/flight/entity/Booking.java
+++ src/main/java/com/booking/flight/entity/Booking.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -48,7 +49,7 @@ public class Booking  {
     @JoinColumn(name = "flight_id")
 	Flight flight;
 	
-	@OneToMany(mappedBy = "booking")
+	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL,CascadeType.PERSIST,CascadeType.MERGE}, mappedBy = "booking")
 	List<Passenger> passengers;
 
 	
